139926859974e5d50da89d543a1ca2a9ae3f50f8
ronamit <amitron5@gmail.com>
2019-08-05 16:53:20 +0000

    update

--- tabular/data_utils.py
+++ tabular/data_utils.py
@@ -2,16 +2,16 @@ from __future__ import division, absolute_import, print_function
 import numpy as np
 
 #------------------------------------------------------------------------------------------------------------~
-def GenerateMDP(N, A, k):
+def GenerateMDP(S, A, k):
     """
     Randomly generate an MDP from the Random-MDP distribution.
 
     For each state-action pair (s; a), the distribution over the next state,  P_{s,a,s'}=P(s'|s,a), is determined by choosing k  non-zero entries uniformly from
-     all N states, filling these k entries with values uniformly drawn from [0; 1], and finally normalizing
+     all S states, filling these k entries with values uniformly drawn from [0; 1], and finally normalizing
 
 
     Parameters:
-    N: number of states
+    S: number of states
     A: number of actions
     k: Number of non-zero entries in each row  of transition-matrix
 
@@ -19,12 +19,50 @@ def GenerateMDP(N, A, k):
     P: [S x A x S] transitions probabilities matrix  P_{s,a,s'}=P(s'|s,a)
     R: [S x A] mean rewards matrix R
     """
-    P = np.zeros((N, A, N))
+    P = np.zeros((S, A, S))
     for a in range(A):
-        for i in range(N):
-            nonzero_idx = np.random.choice(N, k, replace=False)
+        for i in range(S):
+            nonzero_idx = np.random.choice(S, k, replace=False)
             for j in nonzero_idx:
                 P[i, a, j] = np.random.rand(1)
             P[i, a, :] /= P[i, a, :].sum()
-    R = np.random.rand(N, A)
+    R = np.random.rand(S, A)
     return P, R
+
+#------------------------------------------------------------------------------------------------------------~
+def SampleTrajectories(P, R, pi, n, depth, p0=None):
+    """
+    # generate n trajectories
+
+
+    Parameters:
+    P: [S x A x S] transitions probabilities matrix  P_{s,a,s'}=P(s'|s,a)
+    R: [S x A] mean rewards matrix R
+    pi: [S x A]  matrix representing  pi(a|s)
+    n: number of trajectories to generate
+    depth: Length of trajectory
+    p0 (optional) [S] matrix of initial state distribution (default:  uniform)
+    Returns:
+    data: list of n trajectories, each is a list of sequence of depth tuples (state, action, reward, next - state)
+    """
+    S = P.shape[0]
+    A =  P.shape[1]
+    if p0 is None:
+        p0 = np.ones(S) / S #  uniform
+    data = []
+    for i_traj in range(n):
+        data.append([])
+        # sample initial state:
+        s = np.random.choice(S, size=1, p=p0)
+        for t in range(depth):
+            # Until t==depth, sample a~pi(.|s), s'~P(.|s,a), r~R(s,a)
+            a = np.random.choice(A, size=1, p=pi[s, :])
+            s_next = np.random.choice(A, size=1, p=P[s, a, :])
+            r
+            data[i_traj] =
+            s = s_next
+
+
+
+
+# subroutine: SampleTrajectories (see sampleTrajectories.m)
--- tabular/planing_utils.py
+++ tabular/planing_utils.py
@@ -32,20 +32,20 @@ def GetPolicyDynamics(R, P, pi):
     P_pi: [S x S] transitions matrix  when following the policy pi      (P_pi)_{s,s'} P^pi(s'|s)
     R_pi: [S] mean rewards at each state when following the policy pi    (R_pi)_{s} = R^pi(s)
     """
-    N = P.shape[0]
+    S = P.shape[0]
     A = P.shape[1]
-    P_pi = np.zeros((N, N))
-    R_pi = np.zeros((N))
-    for i in range(N): # current state
+    P_pi = np.zeros((S, S))
+    R_pi = np.zeros((S))
+    for i in range(S): # current state
         for a in range(A):
-            for j in range(N): # next state
+            for j in range(S): # next state
                 # Explanation: P(s'|s) = sum_a pi(a|s)P(s'|s,a)
                 P_pi[i, j] += pi[i, a] * P[i,a,j]
             R_pi[i] += pi[i, a] * R[i,a]
     return P_pi, R_pi
 
 #------------------------------------------------------------------------------------------------------------~
-def PolicyEvaluation(R, P, pi, gamma):
+def PolicyEvaluation(R, P, pi, gamma, P_pi=None, R_pi=None):
     """
     Calculates the value-function for a given policy pi and a known model
 
@@ -60,9 +60,16 @@ def PolicyEvaluation(R, P, pi, gamma):
     Q_pi [S x A] The Q-function for a fixed policy pi, i,e. the the expected discounted return when following pi starting from some state and action
     """
     # (1) Use PolicyDynamics to get P and R, (2) V = (I-gamma*P)^-1 * R
-    N = P.shape[0]
-    P_pi, R_pi = GetPolicyDynamics(R, P, pi)
-    V_pi = np.linalg.solve((np.eye(N) - gamma * P_pi).T, P_pi.T).T
-    Q_pi = V_pi
+    S = P.shape[0]
+    A = P.shape[1]
+    if P_pi is None or R_pi is None:
+        P_pi, R_pi = GetPolicyDynamics(R, P, pi)
+    V_pi = np.linalg.solve((np.eye(S) - gamma * P_pi), R_pi)
+    # Verify that R_pi + gamma * np.matmul(P_pi,  V_pi) == V_pi
+    Q_pi = np.zeros((S, A))
+    for a in range(A):
+        for i in range(S):
+            Q_pi[i, a] = R[i, a] + gamma * np.matmul(P[i,a,:], V_pi)
+    # Verify that V_pi(s) = sum_a pi(a|s) * Q_pi(s,a)
     return V_pi, Q_pi
 
