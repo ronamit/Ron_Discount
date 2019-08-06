5cfa19c2fac562502245ccc8451f68dd06f6deaa
ronamit <amitron5@gmail.com>
2019-08-06 13:20:12 +0000

    update

--- tabular/learning_utils.py
+++ tabular/learning_utils.py
@@ -2,3 +2,41 @@ from __future__ import division, absolute_import, print_function
 import numpy as np
 
 #------------------------------------------------------------------------------------------------------------~
+def ModelEstimation(data, S, A):
+    """
+    Maximum-Likelihood estimatiom of model based on data
+
+    Parameters:
+    data: list of n trajectories, each is a list of sequence of depth tuples (state, action, reward, next state)
+    S: number of states
+    A: number of actions
+
+    Returns:
+    P_est: [S x A x S] estimated transitions probabilities matrix  P_{s,a,s'}=P(s'|s,a)
+    R_est: [S x A] estimated mean rewards matrix R
+    """
+
+    counts_sas = np.zeros((S,A,S))
+    counts_sa =  np.zeros((S,A))
+    R_est = np.zeros((S,A))
+    P_est = np.zeros((S,A,S))
+    for traj in data:
+        for sample in traj:
+            (s,a,r,s_next) = sample
+            counts_sa[s,a] += 1
+            counts_sas[s,a,s_next] += 1
+            R_est[s,a] += r
+
+    for s in range(S):
+        for a in range(A):
+            if counts_sa[s,a] == 0:
+                # if this state-action doesn't exist in data
+                # Use default values:
+                R_est[s,a] = 0.5
+                P_est[s,a,:] = 1/S
+            else:
+                R_est[s,a] /= counts_sa[s,a]
+                P_est[s, a, :] = counts_sas[s,a,:] / counts_sa[s,a]
+    if np.any(np.abs(P_est.sum(axis=2) - 1) > 1e-5):
+        raise RuntimeError('Probabilty matrix not normalized!!')
+    return P_est, R_est
--- tabular/planing_utils.py
+++ tabular/planing_utils.py
@@ -36,12 +36,16 @@ def GetPolicyDynamics(R, P, pi):
     A = P.shape[1]
     P_pi = np.zeros((S, S))
     R_pi = np.zeros((S))
-    for i in range(S): # current state
+    for i in range(S):  # current state
         for a in range(A):
             for j in range(S): # next state
                 # Explanation: P(s'|s) = sum_a pi(a|s)P(s'|s,a)
                 P_pi[i, j] += pi[i, a] * P[i,a,j]
+                is_row_updated = True
             R_pi[i] += pi[i, a] * R[i,a]
+
+    if np.any(np.abs(P_pi.sum(axis=1) - 1) > 1e-5):
+        raise RuntimeError('Probabilty matrix not normalized!!')
     return P_pi, R_pi
 
 #------------------------------------------------------------------------------------------------------------~
@@ -73,3 +77,42 @@ def PolicyEvaluation(R, P, pi, gamma, P_pi=None, R_pi=None):
     # Verify that V_pi(s) = sum_a pi(a|s) * Q_pi(s,a)
     return V_pi, Q_pi
 
+#------------------------------------------------------------------------------------------------------------~
+def PolicyIteration(P, R, gamma):
+    """
+       Finds the optimal policy given a known model using policy-iteration algorithm
+
+       Parameters:
+       P: [S x A x S] transitions probabilities matrix  P_{s,a,s'}=P(s'|s,a)
+       R: [S x A] mean rewards matrix R
+       gamma: Discount factor
+
+       Returns
+       pi_opt [S x A]: An optimal policy (assuming given model and gamma)
+       V_opt: [S] The optimal value-function , i,e. the the expected discounted return when following optimal policy  starting from some state
+       Q_opt [S x A] The optimal Q-function, i,e. the the expected discounted return when following optimal policy starting from some state and action
+       """
+
+    # The algorithm: until policy not changes: (1) run policy-evaluation to get Q_pi  (2) new_policy = argmax Q
+    S = P.shape[0]
+    A = P.shape[1]
+    Q_pi = np.zeros((S, A))
+    # initial point of the algorithm: uniform policy
+    pi = np.ones((S, A)) / A
+    pi_prev = A - pi # arbitrary different policy than pi
+    max_iter = S*A
+    iter = 0
+    while np.any(pi != pi_prev):
+        pi_prev = pi
+        _, Q_pi = PolicyEvaluation(R, P, pi, gamma)
+        # Policy improvment:
+        pi = np.zeros((S, A))
+        pi[np.arange(S), np.argmax(Q_pi, axis=1)] = 1  #  set 1 for the optimal action w.r.t Q, and 0 for the other actions
+        if iter > max_iter:
+            raise RuntimeError('Policy Iteration should have stopped by now!')
+        iter += 1
+
+    pi_opt = pi
+    Q_opt = Q_pi
+    V_opt = np.max(Q_opt, axis=1)
+    return pi_opt,  V_opt, Q_opt
